279910a269c5be88378b7370a6d199b6490c5ee2
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-08 11:15:27 -0300

    Commit

--- Contas/ContaPoupanca.java
+++ Contas/ContaPoupanca.java
@@ -1,6 +1,45 @@
-package Contas;
+package CONTA;
 
-public class ContaPoupanca extends Conta{
+public class ContaPoupanca extends Conta {
 
+	int diaAniversarioConta = 5;
+ 
+	public ContaPoupanca() {
+		
+	 }
 	
+	public ContaPoupanca(int numero, String cpf) {
+		super();
+	 }
+    
+	public ContaPoupanca(int diaAniversarioConta) {
+		super();
+		this.diaAniversarioConta = diaAniversarioConta;
+	  }
+
+
+	public void saqueBancario(int saque) {
+		if (saque < getSaldo()) {
+			//getSaldo() = getSaldo() - saque;
+			System.out.println("O seu saque de: " + saque + " foi efetuado com sucesso");
+
+		} else {
+			System.err.println("Saque maior que saldo total.");
+		}
+
+		if (saque == getSaldo()) {
+			System.out.println("O seu saque de: " + saque + " foi efetuado com sucesso");
+		}
+	}
+
+	public void correcao(int niverDaConta) {
+		if (diaAniversarioConta == niverDaConta) {
+			System.out.println("-------> Parabéns o ajuste foi dado em 5%.");
+			String texto = "-------> O seu novo saldo é:  ";
+			System.out.println(texto + ((getSaldo()*0.05) + getSaldo()) + "R$");
+
+		} else {
+			System.err.println("-------> A data não bate, deixou de ganhar 5%.");
+		}
+	}
 }
